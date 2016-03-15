2ab15954558d096f5adf1b0b59532cea84a826a0
Abhishek H P <abhishekhp@Abhisheks-Mac.local>
2016-03-16 01:28:14 +0530

    added character class file

---
+++ FinalAttack/Character.swift
@@ -0,0 +1,49 @@
+//
+//  Character.swift
+//  FinalAttack
+//
+//  Created by Abhishek H P on 3/15/16.
+//  Copyright © 2016 Abhishek H P. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _hp: Int = 100
+    private var _attackPwr: Int = 10
+    
+    var HP: Int {
+        get {
+            return _hp
+        }
+    }
+    
+    var AttackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var isAlive: Bool{
+        get{
+            if HP <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+    init (StartingHP: Int, AttackPower: Int) {
+        self._hp = StartingHP
+        self._attackPwr = AttackPower
+    }
+    
+    func AttemptAttack() -> Bool {
+        self._hp -= AttackPwr
+        
+        return true
+    }
+    
+    
+}
